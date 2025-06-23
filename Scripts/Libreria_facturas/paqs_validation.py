e6002473d943fd75d0f724ec450f6de7d238e406
Jorge Armando Cuaxospa <armjorge@gmail.com>
2025-06-23 17:00:17 -0600

    Liga de facturas y conciliación vs hoja facturas, la función es multi match

--- Scripts/Libreria_facturas/paqs_validation.py
+++ Scripts/Libreria_facturas/paqs_validation.py
@@ -3,41 +3,12 @@ import re
 import glob
 import pandas as pd
 import numpy as np
+from openpyxl import load_workbook
 
-def correccion_types(df_entregas_o_altas, df_facturas, info_types):
-    if info_types == 'IMSS': 
-        print(f"Iniciamos la corrección de tipos para el {info_types}")
-        print(f"Número de filas del dataframe facturas al iniciar {df_facturas.index.size}")
-        print(f"Número de filas del dataframe altas al iniciar {df_entregas_o_altas.index.size}")
-        # El siguiente paso es debido a la existencia de valores infinitos en la columna Referencia
-        df_facturas['Referencia'] = (
-            df_facturas['Referencia']
-            .replace([np.inf, -np.inf], np.nan)  # Inf  → NaN
-            .fillna(0)                           # NaN  → 0
-            .astype('int64')                     # ahora ya solo hay valores válidos para int64
-        )        
 
-        df_facturas['Referencia'] = df_facturas['Referencia'].astype('int64')
-        df_entregas_o_altas['noOrden'] = df_entregas_o_altas['noOrden'].astype('int64')
-       
-        # (II.2) Duplicados. 
-        duplicados_facturas = df_facturas.duplicated().sum()
-        print(f"El dataframe facturas tiene {duplicados_facturas} filas duplicadas, vamos a removerlas\n")
-        df_facturas = df_facturas.drop_duplicates()
-        # (II.3) Ausentes
-        print("Removemos del dataframe facturas aquellas filas con Alta y Orden vacíos\n")
-        mask = ((df_facturas['Referencia'].isna() | (df_facturas['Referencia'] == 0)) & df_facturas['Alta'].isna()
-)
-        print(f"Totales de las filas con Referencia y Alta ausentes = {mask.index.size}")
-        df_facturas = df_facturas.loc[~mask].reset_index(drop=True)
-
-        return df_entregas_o_altas, df_facturas
-
-    else: 
-        print("no considerado aún")   
-
-def validacion_de_paqs(dict_path_sheet, dic_columnas, paq_folder, df_entregas_o_altas, info_types):
+def validacion_de_paqs(dict_path_sheet, dic_columnas, paq_folder, altas_path, info_types):
     # (I) Carga
+    df_entregas_o_altas = pd.read_excel(altas_path, sheet_name='df_altas')
     columnas_objetivo = ["Folio", "Referencia", "Alta", "Total", "UUID"]
     df_facturas = pd.DataFrame(columns=columnas_objetivo)
 
@@ -67,8 +38,115 @@ def validacion_de_paqs(dict_path_sheet, dic_columnas, paq_folder, df_entregas_o_
     print("Información del dataframe de facturas: \n")
     print(df_facturas.info())
     excel_facturas= os.path.join(paq_folder, f"{info_types}.xlsx")
+   
+    # III.I Cargar IMSS y ligar.     
+    df_altas_df_facturas = {
+        'noAlta': 'Alta',
+        'noOrden': 'Referencia'
+    }
+
+    df_entregas_o_altas['Factura'] = multi_column_lookup(
+        df_to_fill=df_entregas_o_altas,
+        df_to_consult=df_facturas,
+        match_columns=df_altas_df_facturas,
+        return_column='Folio',
+        default_value='sin match'
+    )
+    # III.II Cargar IMSS y ligar.     
+    df_altas_df_facturas = {
+        'Alta': 'noAlta',
+        'Referencia': 'noOrden'
+    }
+
+    df_facturas['Alta_ligada'] = multi_column_lookup(
+        df_to_fill=df_facturas,
+        df_to_consult=df_entregas_o_altas,
+        match_columns=df_altas_df_facturas,
+        return_column='noAlta',
+        default_value='alta no localizada'
+    )
+    print(df_entregas_o_altas.sample(20))
+
     df_facturas.to_excel(excel_facturas, index=False)
+    # Cargar el archivo conservando las hojas
+    with pd.ExcelWriter(altas_path, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
+        df_entregas_o_altas.to_excel(writer, sheet_name='df_altas', index=False)
+
     print("\nExcel generado de facturas generado exitosamente\n")
+
+
+
+def multi_column_lookup(df_to_fill, df_to_consult, match_columns: dict, return_column, default_value='sin match'):
+    """
+    Realiza búsqueda con múltiples columnas y retorna valor o advertencias.
+
+    Args:
+        df_to_fill (pd.DataFrame): DataFrame que queremos llenar.
+        df_to_consult (pd.DataFrame): DataFrame que consultamos.
+        match_columns (dict): {col_df_to_fill: col_df_to_consult} pares de columnas para hacer match.
+        return_column (str): Columna en df_to_consult con el valor a traer.
+        default_value (any): Valor si no hay coincidencia.
+
+    Returns:
+        pd.Series: Valores para poblar la columna deseada.
+    """
+
+    if not isinstance(df_to_consult, pd.DataFrame):
+        raise TypeError(f"El argumento 'df_to_consult' debe ser un DataFrame, se recibió: {type(df_to_consult)}")
+
+    result_list = []
+
+    for _, row in df_to_fill.iterrows():
+        # Construir filtro booleano dinámico
+        mask = pd.Series([True] * len(df_to_consult))
+
+        for source_col, consult_col in match_columns.items():
+            mask &= df_to_consult[consult_col] == row[source_col]
+
+        filtered = df_to_consult[mask]
+
+        if filtered.empty:
+            result_list.append(default_value)
+        elif len(filtered) > 1:
+            resultados_duplicados = ", ".join(filtered[return_column].astype(str).tolist())
+            result_list.append(f'Peligro: 2 resultados {resultados_duplicados}')
+        else:
+            result_list.append(filtered.iloc[0][return_column])
+
+    return pd.Series(result_list, index=df_to_fill.index)
+
+
+def correccion_types(df_entregas_o_altas, df_facturas, info_types):
+    if info_types == 'IMSS': 
+        print(f"Iniciamos la corrección de tipos para el {info_types}")
+        print(f"Número de filas del dataframe facturas al iniciar {df_facturas.index.size}")
+        print(f"Número de filas del dataframe altas al iniciar {df_entregas_o_altas.index.size}")
+        # El siguiente paso es debido a la existencia de valores infinitos en la columna Referencia
+        df_facturas['Referencia'] = (
+            df_facturas['Referencia']
+            .replace([np.inf, -np.inf], np.nan)  # Inf  → NaN
+            .fillna(0)                           # NaN  → 0
+            .astype('int64')                     # ahora ya solo hay valores válidos para int64
+        )        
+
+        df_facturas['Referencia'] = df_facturas['Referencia'].astype('int64')
+        df_entregas_o_altas['noOrden'] = df_entregas_o_altas['noOrden'].astype('int64')
+       
+        # (II.2) Duplicados. 
+        duplicados_facturas = df_facturas.duplicated().sum()
+        print(f"El dataframe facturas tiene {duplicados_facturas} filas duplicadas, vamos a removerlas\n")
+        df_facturas = df_facturas.drop_duplicates()
+        # (II.3) Ausentes
+        print("Removemos del dataframe facturas aquellas filas con Alta y Orden vacíos\n")
+        mask = ((df_facturas['Referencia'].isna() | (df_facturas['Referencia'] == 0)) & df_facturas['Alta'].isna())
+        print(f"Totales de las filas con Referencia y Alta ausentes = {mask.index.size}")
+        df_facturas = df_facturas.loc[~mask].reset_index(drop=True)
+
+        return df_entregas_o_altas, df_facturas
+
+    else: 
+        print("no considerado aún")   
+
 
 if __name__ == "__main__":
     # Esto solo se ejecuta cuando corres el script, no cuando lo importas desde otro módulo.
@@ -77,6 +155,6 @@ if __name__ == "__main__":
     dic_columnas = {'IMSS_2023': ['Folio', 'Referencia', 'Alta', 'Total', 'UUID'], 'IMSS_2024': ['Folio', 'Referencia', 'Alta', 'Total', 'UUID'], 'IMSS_2025': ['Folio', 'Referencia', 'Alta', 'Total', 'UUID']} 
     paq_folder = r"C:\Users\arman\Dropbox\3. Armando Cuaxospa\Adjudicaciones\Licitaciones 2025\E115 360\Implementación\Facturas\IMSS"
     altas_path = r"C:\Users\arman\Dropbox\3. Armando Cuaxospa\Adjudicaciones\Licitaciones 2025\E115 360\Implementación\SAI\Ordenes_altas.xlsx"
-    df_entregas_o_altas = pd.read_excel(altas_path, sheet_name='df_altas')
+
     info_types = 'IMSS'
-    validacion_de_paqs(dict_path_sheet, dic_columnas, paq_folder, df_entregas_o_altas, info_types)
+    validacion_de_paqs(dict_path_sheet, dic_columnas, paq_folder, altas_path, info_types)
